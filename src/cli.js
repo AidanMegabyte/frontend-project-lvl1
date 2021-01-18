630da054c6a56b7a1b17d962e91ec0412c9cfc09
Sergei Kuskov <aidan.megabyte@gmail.com>
2021-01-18 22:36:09 +0300

    [+] Добавил Eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greetUser = () => {
-    let userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+export default function greetUser() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 }
